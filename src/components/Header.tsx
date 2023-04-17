851c008dd1b11656502a137afa1f67b8f74e03ae
Alison Wu <alison.wu23@gmail.com>
2023-04-17 11:11:31 +0800

    feat: how react render - Reconciliation

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header: React.FC = () => {
+  return (
+    <header>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,11 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}, 1000);
