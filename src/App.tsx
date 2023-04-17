ecac2a8c8724d22c3324862f38b5ddd526c6f81b
Alison Wu <alison.wu23@gmail.com>
2023-04-17 16:43:00 +0800

    chore: back to Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-
-type BtnType = {
-  colorStatus: boolean
-}
-
-const Button = styled.button<BtnType>`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid ${props => props.colorStatus === false ? 'grey' : 'seagreen'};
-  color: #000;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`
-const H1 = styled.h1`
-  color: blue;
-`
 
 const App: React.FC = () => {
   return <>
-    <H1>Hello</H1>
-    <Button colorStatus={false}>Submit</Button>
+    <h1>Hello World</h1>
   </>
 }
 
