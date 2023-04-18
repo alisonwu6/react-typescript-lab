04df9a5fc906dec69b4de8affad8bdaa3269363f
Alison Wu <alison.wu23@gmail.com>
2023-04-18 15:02:55 +0800

    feat:  useEffect - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App: React.FC = () => {
+  // ＊ Mount => call data from api
+  useEffect(() => {
+    return () => {
+      console.log('Hello useEffect')
+    };
+  }, []);
+  // ---------------------------------------------------------------------------
+
+  const [counter, setCounter] = useState(0);
+  const [text, setText] = useState('Even');
+
+  // ＊ ❌ too many re-renders. 
+  // console.log('text', text)
+  // setText("XXX")
+  // ---------------------------------------------------------------------------
+
+  useEffect(() => {
+    console.log('useEffect -> setText')
+    const currentText = counter % 2 === 0 ? 'Even' : 'Odd';
+    setText(currentText)
+  }, [counter]);
+
+  const counterHandler = () => {
+    setCounter(counter + 1)
+  }
+
   return <>
-    <h1>Hello World</h1>
+    <h1>Counter: { counter }  <span>({ text })</span></h1>
+    <button onClick={ counterHandler }>+1</button>
   </>
 }
 
