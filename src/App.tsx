c93e25d0b9fa1e017604aba9d92cbb23d5dc54fc
Alison Wu <alison.wu23@gmail.com>
2023-04-19 11:49:52 +0800

    feat: useRef 1: maintain values across renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useRef } from 'react'
+// useRef -> let you maintain the same reference across renders.
 
 const App: React.FC = () => {
-  return <>
-    <h1>Hello World</h1>
-  </>
+  const [hidden, setHidden] = useState(false)
+  const sumRef = useRef(0);
+
+  // let sum = 0
+  console.log('-----[Render]', sumRef.current)
+
+  function click() {
+    sumRef.current = sumRef.current + 1
+    console.log('[Click] sumRef.current:', sumRef.current)
+
+    if (sumRef.current === 5) {
+      setHidden(true)
+    } else if (sumRef.current > 15) {
+      setHidden(false)
+    }
+  }
+  return (
+    <>
+      <h1>Ref</h1>
+      <button onClick={click}>+1</button>
+      {hidden && <div>The hidden area</div>}
+    </>
+  )
 }
 
-export default App;
+export default App
