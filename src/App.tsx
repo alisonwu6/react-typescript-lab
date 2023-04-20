e1825fa60442112e1713d7beca74dd1e5458c0b0
Alison Wu <alison.wu23@gmail.com>
2023-04-20 10:52:52 +0800

    feat: hooks: useMemo, useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react'
 
 const App: React.FC = () => {
-  return <>
-    <h1>Hello World</h1>
-  </>
+  console.log('Render')
+
+  const [value, setValue] = useState(false)
+  // const aa = 1000
+
+  /**
+   * The 'obj' object makes the dependencies of useEffect Hook change on every render.
+   * => useMemo to memoize values.
+   * => useCallback to memoize a function.
+   */
+  // const obj = { name: 'Joe', data: { gender: 'male', age: 30 } }
+
+  // useMemo (object, array, function)
+  const memoObj = useMemo(() => {
+    const obj = { name: 'Joe', data: { gender: 'female', age: 30 } }
+    return obj
+  }, [])
+
+  // const memoFunc1 = useMemo(() => {
+  //   return () => {}
+  // }, [])
+
+  // const memoFunc2 = useCallback(() => {
+  //   console.log('here')
+  // }, [])
+
+  // Shallow Compare
+  useEffect(() => {
+    console.log('useEffect')
+  }, [memoObj])
+
+  return (
+    <>
+      <h1>Hooks</h1>
+      <button
+        onClick={() => {
+          setValue(!value)
+        }}
+      >
+        Re-render
+      </button>
+    </>
+  )
 }
 
-export default App;
+export default App
