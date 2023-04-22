51870b51be11c6e420bc49c693e6c7d84a4d7fd8
Alison Wu <alison.wu23@gmail.com>
2023-04-22 12:58:45 +0800

    feat: redux: middleware

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from './slices/todo'
+import { loggerMiddleware } from './middleware'
+import { combineReducers } from 'redux'
+
+const reducers = combineReducers({
+  todoReducer
+})
 
 const store = configureStore({
-  reducer: {
-    todoReducer,
-  },
+  reducer: reducers,
+  middleware: (getCurrentMiddleware) => {
+    return getCurrentMiddleware().concat(loggerMiddleware)
+  }
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof reducers>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
